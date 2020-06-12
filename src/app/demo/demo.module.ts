5f4682e264760a98b7662f1c1566bec657bcc28f
Jonathan Lam <jlam55555@gmail.com>
2020-06-12 15:11:33 -0400

    added demo module and routing

---
+++ src/app/demo/demo.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DemoRoutingModule } from './demo-routing.module';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material/material.module';
+
+@NgModule({
+  declarations: [ButtonsComponent],
+  imports: [
+    CommonModule,
+    DemoRoutingModule,
+    FormsModule,
+    MaterialModule
+  ]
+})
+export class DemoModule { }
